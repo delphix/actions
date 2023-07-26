2b6d618c7cd706c703c750a336a8aa65e6aceda2
Jules Tamagnan <77420472+jtamagnan-delphix@users.noreply.github.com>
2023-07-26 10:07:29 -0700

    TOOL-22158 Add new delete-old-branches action (#14)
    PR URL: https://www.github.com/delphix/actions/pull/14

---
+++ delete-old-branches/README.md
@@ -0,0 +1,112 @@
+# Delete Old Branches Action
+
+This action is meant to cleanup old lingering branches in a
+repository. There are a number of dials that you can play with to only
+select the branches that you want gone.
+
+This action is originally based off of:
+ - https://github.com/icyak/delete-old-branches-action/tree/master
+
+but was translated to Python and tweaked to work better with how
+branches are used at Delphix.
+
+## Inputs
+
+| Input                    | Description                                                  |
+|--------------------------|--------------------------------------------------------------|
+| repo_token               | *Required*: The GITHUB_TOKEN secret                          |
+| date                     | *Required*: A git-log compatible date format. (3 months ago) |
+| dry_run                  | *Required*: Run in dry-run mode so no branches are deleted   |
+| branch_regex             | A regex for branches to include                              |
+| excluded_branches        | A comma seperated list of branch(es) to exclude              |
+| excluded_branch_regex    | A regex for branches to exclude.                             |
+| exclude_open_pr_branches | Exclude branches that have an open pull request              |
+
+## Examples
+
+There are two ways to use the action. You can use the action in your
+own workflow directly, or you can use the reusable workflow defined in
+this repository.
+ - The reusable workflow that consumes this action: [../.github/workflows/delete-old-branches-workflows.yaml](../.github/workflows/delete-old-branches-workflows.yaml)
+ - Post push automation that consumes the reusable workflow [../.github/workflows/delete-old-branches.yaml](../.github/workflows/delete-old-branches.yaml)
+
+### Using the action directly
+
+1. Only run against certain branches via include:
+   ```yaml
+   jobs:
+     housekeeping:
+       name: Cleanup old branches
+       runs-on: ubuntu-latest
+       steps:
+         - name: Checkout repository
+           uses: actions/checkout@v2
+         - name: Run delete-old-branches-action
+           uses: ./delete-old-branches
+           with:
+             repo_token: ${{ github.token }}
+             date: '3 months ago'
+             dry_run: true
+             branch_regex: 'dlpx/.*|projects/.*|dependabot/.*|gh-readonly-queue/.*'
+   ```
+2. Only run against certain branches via exclude:
+   ```yaml
+   jobs:
+     housekeeping:
+       name: Cleanup old branches
+       runs-on: ubuntu-latest
+       steps:
+         - name: Checkout repository
+           uses: actions/checkout@v2
+         - name: Run delete-old-branches-action
+           uses: ./delete-old-branches
+           with:
+             repo_token: ${{ github.token }}
+             date: '3 months ago'
+             dry_run: true
+             excluded_branches: 'develop,patch,release,main'
+             excluded_branch_regex: 'projects/.*'
+   ```
+
+### Using the reusable workflow instead of the action
+
+1. Use the default settings
+   [../.github/workflows/delete-old-branches.yaml](../.github/workflows/delete-old-branches.yaml)
+2. Only run against certain branches via include:
+   ```yaml
+   jobs:
+     delete-old-branches:
+       uses: ./.github/workflows/delete-old-branches-workflow.yml
+       branch_regex: 'dlpx/.*|projects/.*|dependabot/.*|gh-readonly-queue/.*'
+   ```
+3. Only run against certain branches via exclude:
+   ```yaml
+   jobs:
+     delete-old-branches:
+       uses: ./.github/workflows/delete-old-branches-workflow.yml
+       excluded_branches: 'develop,patch,release,main'
+       excluded_branch_regex: 'projects/.*'
+   ```
+
+## Testing
+
+It is possible to test this action using [act](https://github.com/nektos/act).
+
+## Testing by running the workflow
+
+To test it as part of the workflow  you can run:
+```console
+$ act -W .github/workflows/delete-old-branches-workflow.yml -s GITHUB_TOKEN="$GITHUB_TOKEN"
+```
+You can also use `--input` to change some values like:
+```console
+$ act -W .github/workflows/delete-old-branches-workflow.yml -s GITHUB_TOKEN="$GITHUB_TOKEN" --input branch_regex='dlpx/.*|projects/.*|dependabot/.*|gh-readonly-queue/.*'
+```
+
+## Testing by running the post push action that consumes the workflow
+
+N.B. When testing the post-push action you cannot specify any inputs.
+
+```console
+$ act -j housekeeping -s GITHUB_TOKEN="$GITHUB_TOKEN"
+```
---
+++ delete-old-branches/delete_old_branches.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import typing
+import urllib.error
+import urllib.parse
+import urllib.request
+import re
+import email
+import subprocess
+import functools
+
+#
+# Load settings
+#
+class Settings:
+    def __init__(self):
+        print(os.environ)
+        self.base_uri=os.environ["GITHUB_API_URL"]
+        self.workspace=os.environ["GITHUB_WORKSPACE"]
+        self.repository=os.environ["GITHUB_REPOSITORY"]
+
+        self.repo_token=os.environ["INPUT_REPO_TOKEN"]
+        self.date=os.environ["INPUT_DATE"]
+        self.dry_run=os.environ.get("INPUT_DRY_RUN", "true") == "true"
+
+        self.branch_regex=os.environ.get("INPUT_BRANCH_REGEX", "")
+
+        self.excluded_branches = os.environ.get("INPUT_EXCLUDED_BRANCHES", "main,master,develop").split(",")
+        self.excluded_branch_regex=os.environ.get("INPUT_EXCLUDED_BRANCH_REGEX", "")
+        self.exclude_open_pr_branches=os.environ.get("INPUT_EXCLUDE_OPEN_PR_BRANCHES", "true") == "true"
+
+
+SETTINGS = Settings()
+
+#
+# GitHub API client
+#
+class Response(typing.NamedTuple):
+    body: str
+    headers: email.message
+    status: int
+
+    def json(self) -> typing.Any:
+        return json.loads(self.body)
+
+def request(
+    url: str,
+    method: str = "GET",
+) -> Response:
+    """
+    A simple function to make calls to GitHub
+    """
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {SETTINGS.repo_token}"
+    }
+
+    httprequest = urllib.request.Request(
+        url, headers=headers, method=method
+    )
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = Response(
+                headers=httpresponse.headers,
+                status=httpresponse.status,
+                body=httpresponse.read().decode(
+                    httpresponse.headers.get_content_charset("utf-8")
+                ),
+            )
+    except urllib.error.HTTPError as e:
+        response = Response(
+            body=str(e.reason),
+            headers=e.headers,
+            status=e.code,
+        )
+
+    return response
+
+#
+# Branch checks
+#
+def _excluded_branch(branch: str) -> bool:
+    """
+    Return True if the branch is an excluded branch
+    """
+    for excluded_branch in SETTINGS.excluded_branches:
+        if branch == excluded_branch:
+            return True
+
+    return False
+
+
+def _included_branch(branch: str) -> bool:
+    """
+    Return True if the branch should be included based on its name.
+    """
+    return bool(re.fullmatch(SETTINGS.branch_regex, branch))
+
+
+def _excluded_branch_regex(branch: str) -> bool:
+    """
+    Return True if the branch should be excluded based on its name.
+    """
+    return bool(re.fullmatch(SETTINGS.excluded_branch_regex, branch))
+
+
+@functools.cache
+def _open_pull_request_branches() -> list[str]:
+    """
+    Return a cached list of all branches associated with open pull
+    requests.
+    """
+    return [
+        pull["head"]["ref"] for pull in
+        request(f"{SETTINGS.base_uri}/repos/{SETTINGS.repository}/pulls").json()
+    ]
+
+
+def _open_pr_branch(branch: str) -> bool:
+    """
+    Return True if there are any open PRs associdated with this branch
+    """
+    if not SETTINGS.exclude_open_pr_branches:
+        return False
+
+    for pull_request_branch in _open_pull_request_branches():
+        if branch == pull_request_branch:
+            return True
+
+    return False
+
+def _delete_branch(branch):
+    """
+    Delete the branch via the GitHub API
+    """
+    response = request(
+        f"{SETTINGS.base_uri}/repos/{SETTINGS.repository}/git/refs/heads/{branch}",
+        method="DELETE",
+    )
+    if response.status != 204:
+        raise Exception(f"Failed to delete {branch}")
+
+
+def _has_been_updated_recently(branch):
+    """
+    Return True if the repository has been updated "recently"
+    """
+    #
+    # Here we see if the branch has any commits since the given
+    # date. If there is such a commit there will be output.
+    #
+    output = subprocess.check_output(["git", "log", "--oneline", "-1", f"--since='{SETTINGS.date}'", f"origin/{branch}"], text=True)
+    return bool(output)
+
+
+#
+# Main
+#
+def main():
+    #
+    # Define was_dry_run output for the github action
+    #
+    print(f"::set-output name=was_dry_run::{SETTINGS.dry_run}")
+
+    #
+    # Set some git properties
+    #
+    subprocess.check_call(["git", "config", "--global", "--add", "safe.directory", SETTINGS.workspace])
+    subprocess.check_call(["git", "config", "url.https://github.com/.insteadOf", "git@github.com:"])
+
+    #
+    # Find all branches in the repository. First perform a fetch from
+    # upstream then list them.
+    #
+    try:
+        # When running in GitHub actions we are mostly likely given a
+        # a shallow repository. When we fetch we also want to make
+        # sure that the repository is not shallow. If running through
+        # `act` however its possible for the clone to be a complete
+        # clone. In the case where the repository is non-shallow,
+        # unshallow will fail.
+        subprocess.check_call(["git", "fetch", "--all", "--prune", "--unshallow"])
+    except:
+        subprocess.check_call(["git", "fetch", "--all", "--prune"])
+    raw_branches = subprocess.check_output(["git", "ls-remote", "-q", "--heads", "--refs"], text=True)
+    branches = []
+    for line in raw_branches.split("\n"):
+        if line:
+            _sha, ref = line.split("\t")
+            branches.append(ref.removeprefix("refs/heads/"))
+
+    #
+    # Go through all branches and determine if the branch should be
+    # deleted or skipped. If any clause triggers we do not perform the
+    # delete.
+    #
+    deleted_branches = []
+    almost_deleted_branches = []
+    for branch in branches:
+        if not _included_branch(branch):
+            print(f"{branch} is not included. Skipping")
+        elif _excluded_branch(branch):
+            print(f"{branch} is an excluded branch. Skipping")
+        elif _has_been_updated_recently(branch):
+            print(f"{branch} has been updated recently. Skipping")
+        elif _excluded_branch_regex(branch):
+            print(f"{branch} is explicitly protected. Skipping")
+        elif _open_pr_branch(branch):
+            print(f"{branch} has an open pull request. Skipping")
+        else:
+            # There is no reason to skip the deletion so lets delete
+            if SETTINGS.dry_run:
+                print(f"DRY RUN: Deleting {branch}")
+                deleted_branches.append(branch)
+            else:
+                print(f"Deleting {branch}")
+                try:
+                    _delete_branch(branch)
+                    deleted_branches.append(branch)
+                except:
+                    almost_deleted_branches.append(branch)
+
+    if almost_deleted_branches:
+        raise Exception(f"A few branches were not deleted [{','.join(almost_deleted_branches)}]")
+
+    print(f"::set-output name=deleted_branches::[{','.join(deleted_branches)}]")
+
+if __name__ == "__main__":
+    main()
